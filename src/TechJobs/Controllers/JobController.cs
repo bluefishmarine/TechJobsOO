f86284c5d2290994e6dd0c619fb6b10da1b8d656
Gary <garyivanov89@gmail.com>
2018-03-19 20:57:37 -0400

    finished TechJobsOO

--- src/TechJobs/Controllers/JobController.cs
+++ src/TechJobs/Controllers/JobController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using TechJobs.Data;
 using TechJobs.ViewModels;
+using TechJobs.Models;
 
 namespace TechJobs.Controllers
 {
@@ -20,7 +21,9 @@ namespace TechJobs.Controllers
         {
             // TODO #1 - get the Job with the given ID and pass it into the view
 
-            return View();
+            Job someJob = jobData.Find(id);
+
+            return View(someJob);
         }
 
         public IActionResult New()
@@ -35,6 +38,29 @@ namespace TechJobs.Controllers
             // TODO #6 - Validate the ViewModel and if valid, create a 
             // new Job and add it to the JobData data store. Then
             // redirect to the Job detail (Index) action/view for the new Job.
+
+            if (ModelState.IsValid)
+            {
+                Job newJob = new Job
+                {
+                    Name = newJobViewModel.Name,
+                    Employer = jobData.Employers.Find(newJobViewModel.EmployerID),
+                    Location = jobData.Locations.Find(newJobViewModel.LocationID),
+                    CoreCompetency = jobData.CoreCompetencies.Find(newJobViewModel.CoreCompetencyID),
+                    PositionType = jobData.PositionTypes.Find(newJobViewModel.PositionTypeID)
+                };
+
+                // Add the new cheese to my existing cheeses
+                jobData.Jobs.Add(newJob);
+
+                return Redirect(string.Format("/Job?id={0}",newJob.ID));
+
+            }
+
+            return View(newJobViewModel);
+
+
+
 
             return View(newJobViewModel);
         }
